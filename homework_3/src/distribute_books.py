b693e595f92274cb475173b616f7d4249639768d
bob-arctor <f.mymind777@gmail.com>
2023-06-12 01:45:48 +0300

    fix homework_3

--- homework_3/src/distribute_books.py
+++ homework_3/src/distribute_books.py
@@ -10,21 +10,29 @@ from homework_3.files import RESULT_FILE_PATH
 def parse_users() -> list:
     with open(USERS_FILE_PATH, "r") as f:
         reader = json.load(f)
-        new_users_list = []
-        for row in reader:
-            user = {'name': row['name'], 'gender': row['gender'], 'address': row['address'], 'age': row['age'],
-                    'books': []}
-            new_users_list.append(user)
+        new_users_list = [
+            {
+                'name': row['name'],
+                'gender': row['gender'],
+                'address': row['address'],
+                'age': row['age'],
+                'books': []
+             } for row in reader
+        ]
         return new_users_list
 
 
 def parse_books() -> list:
     with open(BOOKS_FILE_PATH, newline='') as f:
         reader = DictReader(f)
-        new_books_list = []
-        for row in reader:
-            book = {'title': row['Title'], 'author': row['Author'], 'pages': int(row['Pages']), 'genre': row['Genre']}
-            new_books_list.append(book)
+        new_books_list = [
+            {
+                'title': row['Title'],
+                'author': row['Author'],
+                'pages': int(row['Pages']),
+                'genre': row['Genre']
+            } for row in reader
+        ]
         return new_books_list
 
 
@@ -39,25 +47,25 @@ def distribute_books(users: list, books: list):
     generate_user = generator(users)
     generate_book = generator(books)
     books_count = 0
-    while books_count < len(books):
+    for book in generate_book:
         if books_count < len(users):
             user = next(generate_user)
-            book = next(generate_book)
             user['books'].append(book)
             result_list.append(user)
             books_count += 1
-        elif books_count >= len(users):
-            for user in result_list:
-                if books_count < len(books):
-                    book = next(generate_book)
-                    user['books'].append(book)
-                    books_count += 1
+        else:
+            result_list[books_count % len(users)]['books'].append(book)
+            books_count += 1
+    return result_list
 
+
+def create_result_file(result_file):
     with open(RESULT_FILE_PATH, "w") as f:
-        s = json.dumps(result_list, indent=4)
+        s = json.dumps(result_file, indent=4)
         f.write(s)
 
 
 users_list = parse_users()
 books_list = parse_books()
-distribute_books(users_list, books_list)
+result = distribute_books(users_list, books_list)
+create_result_file(result)
